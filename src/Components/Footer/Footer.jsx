10c271937a49ab2a6c2ca976bd229381c4952e08
henryjobel <jhobelsaha@gmail.com>
2023-09-20 03:00:41 +0600

    path done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+            <h1>This Is A footer</h1>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,26 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
+    children:[
+      {
+        path: "/about",
+        element: <About></About>
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>
+      }
+    ]
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
